6ea7919b32d89afc3ce4171d97382af48b81d6da
Ziqi Wang <ziqi.wang39@gmail.com>
2021-06-06 11:23:38 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Weather-Journal App Project
+
+The purpose of this project is to create an asynchronous web app that uses Web API and user data to dynamically update the UI for a Weather-Journal App.
+
+Coding Steps
+- Setup Node environment with Express
+- Add required dependencies
+- Setup server with GET and POST routes
+- Create developer credentials for Web API
+- Use the Fetch API with my credentials and user input to get dynamic data into my app routes
+- Access a GET route on the server side, from a function called on the client side
+- Chain Promises together
+- Access HTML elements with JavaScript and set their properties dynamically
+- Fix errors
+- Clean up code
---
+++ server.js
@@ -0,0 +1,53 @@
+// Setup empty JS object to act as endpoint for all routes
+//projectData = [];
+projectData = {};
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+//GET route
+
+app.get('/all',(req,res)=>{
+    res.send(projectData);
+    console.log(projectData);
+
+});
+
+//POST route
+app.post('/add',(req,res)=>{
+    let data = req.body;
+    console.log('server side data', data);
+    newEntry = {
+        date: req.body.date,
+        city:req.body.city,
+        temp:req.body.temp,
+        feels_like:req.body.feels_like,
+        content:req.body.content,
+        
+    }
+    //projectData.push(newEntry);
+    projectData = newEntry;
+    console.log(projectData);
+})
+// Setup Server
+
+
+const port = 8080;
+const server = app.listen(port, listening);
+function listening(){
+    console.log(`Server is running on localhost:${port}`);
+    
+}
